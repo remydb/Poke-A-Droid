cfa4669c70984e3339137804901bbf961ec43da1
Remy de Boer <rboer@fx160-03.students.os3.nl>
2013-05-06 15:21:49 +0200

    I HAVE NO IDEA WHAT I'M DOING

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import kivy
 import subprocess
 #import android
-kivy.require('1.0.7')
+kivy.require('1.6.1')
 
 from kivy.app import App
 from kivy.uix.button import Button
@@ -13,7 +13,8 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.accordion import Accordion, AccordionItem
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
-from kivy.uix.camera import Camera
+#from kivy.uix.camera import Camera
+from kivy.core.camera import Camera as CoreCamera
 #from kivy.uix.anchorlayout import AnchorLayout
 
 class SploitApp(App):
@@ -130,19 +131,32 @@ class SploitApp(App):
 
 	def doscreenshot(self,*largs):
 		Window.screenshot(name='screenshot%(counter)04d.jpg')
-     
-	def testcam(self,*largs):
-		camwidget = Widget()  #Create a camera Widget
-		cam = Camera()        #Get the camera
-		cam=Camera(resolution=(640,480), size=(500,500))
-		cam.play=True         #Start the camera
-		camwidget.add_widget(cam)
-		
-		button=Button(text='screenshot',size_hint=(0.12,0.12))
-		button.bind(on_press=self.doscreenshot)
-		camwidget.add_widget(button)    #Add button to Camera Widget
 
-		return camwidget
+	def testcam(self,*largs):
+		#camwidget = Widget()  #Create a camera Widget
+		#cam = Camera()        #Get the camera
+		#cam=Camera(resolution=(640,480))
+		#cam.play=True         #Start the camera
+		#camwidget.add_widget(cam)
+		#################################
+		# I HAVE NO IDEA WHAT I'M DOING #
+		#################################
+		self._camera = CoreCamera(index=0,
+						resolution=(640,480), stopped=False)
+		self._camera.bind(on_load=self._camera_loaded)
+		#img = Image(self.texture)
+		#img.save('pic.png')
+		#button=Button(text='screenshot',size_hint=(0.12,0.12))
+		#button.bind(on_press=self.doscreenshot)
+		#camwidget.add_widget(button)    #Add button to Camera Widget
+		#self.root.add_widget(camwidget)
+		return
+
+	def _camera_loaded(self, *largs):
+		self.texture = self._camera.texture
+		self.texture_size = list(self.texture.size)
+		img = Image(self.texture)
+		img.save('what.png')
 
 	def build(self):
 		acc = Accordion()
