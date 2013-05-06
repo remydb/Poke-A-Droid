1a15f8705e7f01130a686e5390b19fd071c6e43c
Remy de Boer <rboer@fx160-03.students.os3.nl>
2013-05-06 14:01:40 +0200

    Testing new camera stuff

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import kivy
 import subprocess
-import android
+#import android
 kivy.require('1.0.7')
 
 from kivy.app import App
@@ -12,6 +12,8 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.accordion import Accordion, AccordionItem
 from kivy.uix.image import Image
+from kivy.uix.widget import Widget
+from kivy.uix.camera import Camera
 #from kivy.uix.anchorlayout import AnchorLayout
 
 class SploitApp(App):
@@ -120,11 +122,27 @@ class SploitApp(App):
 		popup.open()
 		return
 
-	def testcam(self, value):
-		path = './'
-		path += time.strftime("%B-%_e-%_I-%M-")
-		path += '.png'
-		android.cameraCapturePicture(path, True)
+	# def testcam(self, value):
+	# 	path = './'
+	# 	path += time.strftime("%B-%_e-%_I-%M-")
+	# 	path += '.png'
+	# 	android.cameraCapturePicture(path, True)
+
+	def doscreenshot(self,*largs):
+		Window.screenshot(name='screenshot%(counter)04d.jpg')
+     
+	def testcam(self,*largs):
+		camwidget = Widget()  #Create a camera Widget
+		cam = Camera()        #Get the camera
+		cam=Camera(resolution=(640,480), size=(500,500))
+		cam.play=True         #Start the camera
+		camwidget.add_widget(cam)
+		
+		button=Button(text='screenshot',size_hint=(0.12,0.12))
+		button.bind(on_press=self.doscreenshot)
+		camwidget.add_widget(button)    #Add button to Camera Widget
+
+		return camwidget
 
 	def build(self):
 		acc = Accordion()
